afffc0875d1de2e1aa5f10981e453a4535b1117f
Ezerway <administrator@ezerway.com>
2020-05-10 15:10:58 +0700

    Initial Commit

---
+++ objcallprinter/objcallprinter.h
@@ -0,0 +1,13 @@
+//
+//  objcallprinter.h
+//  objcallprinter
+//
+//  Created by Bob on 5/10/20.
+//  Copyright © 2020 Ezerway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface objcallprinter : NSObject
+
+@end
---
+++ objcallprinter/objcallprinter.m
@@ -0,0 +1,13 @@
+//
+//  objcallprinter.m
+//  objcallprinter
+//
+//  Created by Bob on 5/10/20.
+//  Copyright © 2020 Ezerway. All rights reserved.
+//
+
+#import "objcallprinter.h"
+
+@implementation objcallprinter
+
+@end
